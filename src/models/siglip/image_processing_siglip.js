07e3e18b8c18bf802aa1c961216d47dcfeee859b
Asapanna Rakesh <45640029+rozeappletree@users.noreply.github.com>
2025-08-13 05:15:54 +0530

    rename SiglipImageProcessor Siglip2ImageProcessor

--- src/models/siglip/image_processing_siglip.js
+++ src/models/siglip/image_processing_siglip.js
@@ -2,4 +2,4 @@ import {
     ImageProcessor,
 } from "../../base/image_processors_utils.js";
 
-export class SiglipImageProcessor extends ImageProcessor { }
+export class Siglip2ImageProcessor extends ImageProcessor { }
